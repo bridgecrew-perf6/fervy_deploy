22939e39347f50c08733c3c8cff5456bd83718f9
Esquizo DEV <75645447+EsquizoDEV@users.noreply.github.com>
2022-01-15 12:07:29 -0600

    Delete src directory

--- src/components/FerbySuspense/FerbySuspense.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Suspense } from 'react'
-import FerbyLoading from '../FerbyLoading/FerbyLoading'
-
-const FerbySuspense = ({ children }) => {
-    return <Suspense fallback={<FerbyLoading />}>{children}</Suspense>
-}
-
-export default FerbySuspense
