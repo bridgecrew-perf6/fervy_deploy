2cf502293373681ca2491ac351548c9ba09a9b1d
Esquizo DEV <75645447+EsquizoDEV@users.noreply.github.com>
2022-01-15 12:16:10 -0600

    Add files via upload

---
+++ src/views/sessions/register/components/Step3.jsx
@@ -0,0 +1,10 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+export const Step3 = () => {
+    return (
+        <>
+          <TextField label="Introduce una contraseña"/>
+        </>
+    )
+}
